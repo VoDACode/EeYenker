3b2aad6ff4ae33480a036fa06ac4b151816dc5bb
Karen <timoshchavlada@gmail.com>
2025-01-10 23:11:26 +0200

    Rename search controller and update dependencies

--- src/main.go
+++ src/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"           // Для маршрутизації
 	"EeYenker/src/routes"
+
+	"github.com/gin-gonic/gin" // Для маршрутизації
 )
 
 func main() {
 	r := gin.Default()
 
 	// Завантаження шаблонів
-	
+
 	r.LoadHTMLGlob("src/templates/**/*")
 
 	r.Static("/static", "./src/static")
+	r.Static("/assets", "./src/assets")
 
 	// Реєстрація маршрутів
 	routes.RegisterRoutes(r)
 	routes.RegisterAPI(r)
 
 	// Запуск сервера
-	r.Run("0.0.0.0:8080")
+	r.Run("0.0.0.0:8070")
 }
