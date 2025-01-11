50b46b99ae2a744fb67b98697e9088442ed21011
Karen <timoshchavlada@gmail.com>
2025-01-11 14:20:32 +0200

    Update main.go with color package and print server URL

--- src/main.go
+++ src/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"EeYenker/src/routes"
+	"fmt"
 
-	"github.com/gin-gonic/gin" // Для маршрутизації
+	"github.com/fatih/color" // Для кольорового тексту
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
 	// Завантаження шаблонів
-
 	r.LoadHTMLGlob("src/templates/**/*")
 
 	r.Static("/static", "./src/static")
@@ -20,6 +21,14 @@ func main() {
 	routes.RegisterRoutes(r)
 	routes.RegisterAPI(r)
 
+	// Виведення адреси сервера в консоль із підсвіткою
+	port := "8070"
+	url := fmt.Sprintf("http://localhost:%s", port)
+
+	// Використовуємо кольоровий текст
+	c := color.New(color.FgHiGreen, color.Bold) // Високий зелений текст, жирний
+	c.Printf("Сервер запущено на: %s\n", url)
+
 	// Запуск сервера
-	r.Run("0.0.0.0:8070")
+	r.Run("0.0.0.0:" + port)
 }
